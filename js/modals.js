b5932776bbcd0f26b85d3df662cde390cfb0b3d2
Gonzalo Zamorano <zamoranog256@hotmail.com>
2020-03-13 11:50:49 -0300

    Modal js, faltan arreglos

---
+++ js/modals.js
@@ -0,0 +1,47 @@
+function cambiarSeccion(){
+  var selectValue = document.getElementById("selectVerSeccion");
+  var seccion = selectValue.value;
+
+  if (seccion != 'selectedOption'){
+  	document.getElementById("formAñadirProd").style.display="block";
+  } else {
+  	document.getElementById("formAñadirProd").style.display="none";
+  }
+}
+
+function selectDinamico(){
+  //Conseguir valor del primer select: Seccion
+  var selectValue = document.getElementById("selDinamico");
+  var seccion = selectValue.value;
+
+  //Conseguir valor del segundo select: Productos (hide)
+  var selectedValue2 = document.getElementById("selectBorrarProducto");
+
+  if (seccion != 'selectedOption'){
+
+  	selectedValue2.style.display="block";
+  	
+  } else {
+  	selectedValue2.style.display = "none";
+  	
+  	var btnAction = document.getElementsByClassName("btn-close")[0]; 
+
+  	if(btnAction.disabled == false){
+  			btnAction.disabled = true;
+  		} 
+  }
+
+}
+
+function btnOn(){
+
+	var selectBorrar = document.getElementById("selectBorrarProducto").value;
+	var btnAction = document.getElementsByClassName("btn-close")[0]; 
+
+  	if(selectBorrar != 'selectedOption'){
+  			btnAction.disabled = false;
+  		} else {
+  			btnAction.disabled = true;
+  		}	
+ }
+
